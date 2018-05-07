9abd409f0f466634616165909d3438383c401055
Masin Al-Dujaili <masin.al-dujaili@wikimedia.de>
2018-05-07 15:12:48 +0200

    Init

---
+++ eva.snippets.js
@@ -0,0 +1,63 @@
+var snippets = {
+	"eintritt": {
+		'allgemein': {
+			"einleitung": "<p>%s meldet den <strong>Eintritt</strong> einer neuen mitarbeitenden Person zu %s:</p>\n<ul>\n<li>Rufname: <strong>%s</strong></li>\n<li>Familienname: <strong>%s</strong></li>\n<li>Bereich: <strong>%s</strong></li>\n</ul>\n",
+			'betreff': 'EVA ausgefüllt für neue*n Mitarbeiter*in: %s',
+			"kommProfil": "<p><strong>%s</strong> nimmt die Arbeit am <strong>%s</strong> als <strong>%s</strong> auf.\nDas Kommunikationsprofil für die Stelle ist folgendermaßen definiert: <strong>%s</strong>.</p>\n",
+			"ersteAnwesenheit": "<p>%s wird erstmalig am %s in der Geschäftsstelle sein.</p>",
+		},
+		'it': {
+			"einleitung": "%s meldet den Eintritt einer neuen mitarbeitenden Person zu %s:\n* Rufname: %s\n* Familienname: %s\n* Bereich: %s\n",
+			"hardware": "Hardware-Ausstattung:\n* Rechner: %s\n* Betriebssystem: %s\n* Anzahl Bildschirme: %s\n* Mobiltelefon: %s\n* Festnetztelefon: %s\n* weitere Hardware: %s\n",
+			"software": "Software-Ausstattung:\n* kostenpflichtige Software:\n%s",
+			"zugaenge": "Zugänge:\n%s\n",
+			"mailinglisten": "Mailinglisten:\n%s\n",
+			"betreff": "IT und Zugänge für neue*n Mitarbeiter*in: %s",
+			"ersteAnwesenheit": "%s wird erstmalig am %s in der Geschäftsstelle sein. Bis dahin muss dieser Arbeitsplatz eingerichtet sein: %s",
+		},
+		'office': {
+			"bvg": "<p>%s benötigt <strong>ab %s ein %s-BVG-Ticket</strong>.</p>\n",
+			"ausstattung": "<p>Ausstattung:</p>\n%s\n",
+			"betreff": "Arbeitsmaterialien für neue*n Mitarbeiter*in: %s"
+			"ersteAnwesenheit": "<p>%s wird erstmalig am %s in der Geschäftsstelle sein. Bis dahin muss dieser Arbeitsplatz eingerichtet sein: <strong>%s</strong></p>",
+		},
+		'kommunikation': {
+			'betreff': 'Einführung in die Kommunikation bei WMDE für neue*n Mitarbeiter*in: ',
+		},
+		'if': {
+			'betreff': 'Einführung in die Community für neue*n Mitarbeiter*in: %s',
+		},
+		'personal': {
+		},
+		'vorstand': {
+		}
+	},
+	"veraenderung": {
+		"allgemein": {
+			'einleitung': "<p>%s meldet eine Änderung zu einer mitarbeitenden Person zu %s:</p>\n<ul>\n<li>Rufname: <strong>%s</strong></li>\n<li>Familienname: <strong>%s</strong></li>\n<li>Bereich: <strong>%s</strong></li>\n<li>Änderung: <strong>%s</strong></li>\n</ul>\n",
+		}
+		"stellenwechsel": {
+			'allgemein': {
+				'einleitung': "<p>%s meldet einen internen Stellenwechsel zu einer mitarbeitenden Person zu %s:</p>\n<ul>\n<li>Rufname: <strong>%s</strong></li>\n<li>Familienname: <strong>%s</strong></li>\n<li>bisheriger Bereich: <strong>%s</strong></li>\n<li>neuer Bereich: <strong>%s</strong></li>\n<li>Kommunikationsprofil: <strong>%s</strong></li>\n</ul>\n",
+				"neuerSitzplatz": "<p>%s wird die neue Stelle an diesem Sitzplatz ausfüllen: %s</p>",
+				"letzteAnwesenheit": "<li>Letzter Tag Anwesenheit: <strong>%s</strong></li>\n<li>Voraussichtliche Rückkehr: <strong>%s</strong></li>\n",
+			},
+			'it': {
+				'einleitung': "%s meldet einen internen Stellenwechsel zu einer mitarbeitenden Person zu %s:\n* Rufname: %s\n* Familienname: %s\n* bisheriger Bereich: %s\n* neuer Bereich: %s\n* Kommunikationsprofil: %s\n",
+				"neuerSitzplatz": "%s wird die neue Stelle an diesem Sitzplatz ausfüllen: %s",
+			,}
+			'office': {
+				"genehmigteAusstattung": "<p>Folgende Ausstattung soll %s für die neue Stelle verwenden</p>:\n%s\n\",
+				"nichtGenehmigteAusstattung": "<p>Prüfe bitte, ob %s folgende Ausstattung hat und jetzt abgeben muss:</p>\n<ul>\n%s\n</ul>\n"
+			}
+		}
+		'office':{
+			"genehmigteAusstattung": "<p>Folgende Ausstattung soll %s während dieser Zeit verwenden können:</p>\n<ul>\n%s\n</ul>\n",
+			"nichtGenehmigteAusstattung": "<p>Prüfe bitte, ob %s folgende Ausstattung hat und jetzt abgeben muss:</p>\n<ul>\n%s\n</ul>\n"
+		},
+	},
+	"austritt": {
+		"Allgemein": "<p>%s meldet den Austritt einer mitarbeitenden Person zu %s (Vertragsende: %s):</p>\n<ul>\n<li>Rufname: <strong>%s</strong></li>\n<li>Familienname: <strong>%s</strong></li>\n<li>Bereich: <strong>%s</strong></li>\n<li>Grund: <strong>%s</strong>\n</ul>\n\",
+	}
+
+}
